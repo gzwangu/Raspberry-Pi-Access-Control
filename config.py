f15d45b2508c133b098ea5a969668ff33cacc7a3
Guangzhou Wang <59857595+gzwangu@users.noreply.github.com>
2020-06-06 21:29:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import logging
 from datetime import timedelta
 
-
 class Config:
     # 调试模式
     DEBUG = True
@@ -10,19 +9,16 @@ class Config:
     # session过期时间
     PERMANENT_SESSION_LIFETIME = timedelta(days=7)
 
-
 class DevelopmentConfig(Config):
     # 开发模式配置
     DEBUG = True
     LOG_LEVEL = logging.DEBUG
-
 
 class ProductionConfig(Config):
     # 上线配置
     # 关闭调试
     DEBUG = False
     LOG_LEVEL = logging.ERROR  # 日志级别
-
 
 # 配置字典，键：配置
 config_dict = {
